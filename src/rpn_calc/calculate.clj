59b57ff858a867b6f4e26f4336f16af35951e2ce
Jan Felix Langenbach <o.hase3@gmail.com>
2019-02-16 20:05:36 +0100

    Add compute for strings.

--- src/rpn_calc/calculate.clj
+++ src/rpn_calc/calculate.clj
@@ -1,4 +1,5 @@
-(ns rpn-calc.calculate)
+(ns rpn-calc.calculate
+  (:require [clojure.string :as str :only split]))
 
 (defn apply-stack
   "Pops the given number of arguments from the stack and pushes the result from applying func on them."
@@ -16,6 +17,11 @@
              [#(Double/parseDouble input) 0])))
 
 (defn compute
-  "Reduces a sequence of rpn symbols to its numerical value."
+  "Reduces a sequence of rpn symbols to its numerical values."
   [symbols]
   (reduce #((evaluate %2) %1) [] symbols))
+
+(defn compute-str
+  "Splits a string and computes its values like compute."
+  [input]
+  (compute (str/split input #" ")))
