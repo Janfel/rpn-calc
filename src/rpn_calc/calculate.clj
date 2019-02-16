1d8fe68d7c73e7bb3ec92ef8ac0f3550d8a1b10f
Jan Felix Langenbach <o.hase3@gmail.com>
2019-02-16 19:00:49 +0100

    Add apply-stack.

--- src/rpn_calc/calculate.clj
+++ src/rpn_calc/calculate.clj
@@ -1,1 +1,7 @@
 (ns rpn-calc.calculate)
+
+(defn apply-stack
+  "Pops the given number of arguments from the stack and pushes the result from applying func on them."
+  [[func arity] stack]
+  (let [[head tail] (split-at arity stack)]
+    (conj tail (apply func head))))
