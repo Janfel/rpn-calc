a103389de458315993d61d854f85a096d3b7f26e
Jan Felix Langenbach <o.hase3@gmail.com>
2019-02-16 19:16:25 +0100

    Add evaluation.

--- src/rpn_calc/calculate.clj
+++ src/rpn_calc/calculate.clj
@@ -5,3 +5,20 @@
   [[func arity] stack]
   (let [[head tail] (split-at arity stack)]
     (conj tail (apply func head))))
+
+(defn- num-fn
+  "Wraps a number in an arity 1 function."
+  [num] (fn [_] num))
+
+(defn- parse-num-fn
+  "Parses a number from a string and wraps it like num-fn."
+  [input] (num-fn (Double/parseDouble input)))
+
+(defn evaluate
+  "Evaluates input as a rpn symbol and returns a function that manipulates the number stack accordingly."
+  [input]
+  (partial apply-stack
+           (case input
+             "+" [+ 2]
+             "-" [- 2]
+             [(parse-num-fn input) 0])))
